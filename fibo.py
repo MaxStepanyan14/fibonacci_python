625ec14114fbee5f14722d813ffc921fb92bce64
MaxStepanyan14 <139002580+MaxStepanyan14@users.noreply.github.com>
2023-07-15 13:52:35 +0400

    Update fibo.py

--- fibo.py
+++ fibo.py
@@ -1,1 +1,13 @@
-f1, f2 = 0, 1
+f1, f2 = 0,1
+
+n = int(input("Enter a number: "))
+
+print(f1)
+print(f2)
+
+while n > 2:
+    f1, f2 = f2, f1 + f2
+    print(f2, end="\n")
+    n -=1
+
+input("\nEnter")
